29b6739cd69283499ef7228c3dabbcc82286a3e1
Ali Haidar <alyhdr@Alis-MacBook-Pro.local>
2020-11-20 21:43:17 +0100

    intial commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express')
+var fs = require("fs");
+const app = express();
+const port = 8000;
+var bodyParser = require('body-parser');
+const cors = require('cors');
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+app.use(express.static(__dirname + '/public'));
+
+app.get("/",(req,res)=>{
+  var number = 0
+  fs.readFile("temp.txt", (err, data) =>{
+    if (err) { console.log(err) }
+    result = parseInt(data.toString()) +1
+    res.render("home",{
+      number: result
+    })
+    fs.writeFile("temp.txt", result+"", (err) => {
+      if (err) console.log(err);
+    });
+  });
+})
+app.listen(port, () => {
+  console.log(`listening on port ${port}!`)
+});
